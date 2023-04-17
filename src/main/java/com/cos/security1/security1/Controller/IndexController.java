d969e23fb871fc4dd0e549f3a5cfd6bf4304b4d0
KimGeonWoo <ikkw2040@naver.com>
2023-04-18 02:39:27 +0900

    SpringSecurity 설정

--- src/main/java/com/cos/security1/security1/Controller/IndexController.java
+++ src/main/java/com/cos/security1/security1/Controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.security1.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -9,5 +10,36 @@ public class IndexController {
     @GetMapping({"", "/"})
     public String index(){
         return "index";
+    }
+
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입 완료됨";
     }
 }
